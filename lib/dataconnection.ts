251e02d06f3384e441d2e20101e693be882573bf
Leon Ahuja <leon.ahuja@tandm.ai>
2023-03-15 20:07:29 +1000

    test

--- lib/dataconnection.ts
+++ lib/dataconnection.ts
@@ -334,6 +334,7 @@ export class DataConnection
 	private _sendChunks(blob: Blob): void {
 		const blobs = util.chunk(blob);
 		logger.log(`DC#${this.connectionId} Try to send ${blobs.length} chunks...`);
+		logger.log(`Hey man`);
 
 		for (let blob of blobs) {
 			logger.log(`DC#${this.connectionId}: Enqueing new chunk to buffer ${blob}`);
